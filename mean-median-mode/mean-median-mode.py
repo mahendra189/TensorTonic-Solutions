6d9610d722172593870a099dfc4468c76cb5498d
Mahendrakumar <160317468+mahendra189@users.noreply.github.com>
2026-05-07 21:35:28 +0530

    Update solution for mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -6,6 +6,7 @@ def mean_median_mode(x):
     Compute mean, median, and mode.
     """
     # Write code here
-    X = np.array(x)
-    freq = Counter(x)
-    return np.mean(X),np.median(X),min(freq)
+    values,counts = np.unique(x,return_counts=True)
+    mode = values[np.argmax(counts)]
+    
+    return np.mean(x),np.median(x),mode
