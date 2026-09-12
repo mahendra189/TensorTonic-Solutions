53c8a7dd6124e6c553fd519fe2f1ca0c0b27d1f2
Mahendrakumar <160317468+mahendra189@users.noreply.github.com>
2026-09-13 01:03:10 +0530

    Solve cyclic-encoding

---
+++ cyclic-encoding/cyclic-encoding.py
@@ -0,0 +1,14 @@
+import math
+
+def cyclic_encoding(values: list, period: float) -> list:
+    """
+    Returns the sine and cosine encoding of every cyclic value.
+    """
+    # Write code here
+    return [
+        [
+            math.sin((2*math.pi*v)/period),
+            math.cos((2*math.pi*v)/period)
+        ]
+        for v in values
+    ]
