681d2dab371342912d32f63b1b58821c83b9aba8
Mahendrakumar <160317468+mahendra189@users.noreply.github.com>
2026-09-13 14:44:15 +0530

    Solve expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -1,12 +1,10 @@
 import numpy as np
 
-def expected_value_discrete(x, p):
+def expected_value_discrete(x: list, p: list) -> float:
     """
-    Returns: float expected value
+    Returns the expected value as a Python float.
     """
     # Write code here
     x = np.array(x)
     p = np.array(p)
-    if np.sum(p) != 1:
-        raise ValueError("invalid Probabilites")
-    return np.sum(x@p)
+    return float(np.sum(x*p))
