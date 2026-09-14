2eee1ddc27218404162cd66a9984edeba06a26dc
Mahendrakumar <160317468+mahendra189@users.noreply.github.com>
2026-09-15 00:00:06 +0530

    Solve simple-moving-average

--- simple-moving-average/simple-moving-average.py
+++ simple-moving-average/simple-moving-average.py
@@ -1,3 +1,4 @@
+import math
 def simple_moving_average(values: list, window_size: int) -> list:
     """
     Returns the mean of every complete sliding window.
@@ -6,6 +7,5 @@ def simple_moving_average(values: list, window_size: int) -> list:
     sma = []
     k = window_size
     for i in range(0,len(values)-k+1):
-        print(i)
         sma.append(sum(values[i:i+k])/k)
     return sma
